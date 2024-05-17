96e034976c84223cb047f3d2efecdae9e804360a
beerappabvgp <beerappabharathb@gmail.com>
2024-05-17 21:06:00 +0530

    Completed building student intro project.

--- models/serialize/StudentIntro.ts
+++ models/serialize/StudentIntro.ts
@@ -1,4 +1,40 @@
+import * as borsh from '@project-serum/borsh'
 
 export class StudentIntro {
-   
+  constructor(public name : string , public message : string) {
+    this.name = name;
+    this.message = message;
+  }
+  // serializing the schema 
+  borshInstructionSchema = borsh.struct([
+    borsh.u8('variant'),
+    borsh.str('name'),
+    borsh.str('message'),
+  ]);
+
+  static borshAccountSchema = borsh.struct([
+    borsh.u8('initialized'),
+    borsh.str('name'),
+    borsh.str('message'),
+  ]);
+
+  serialize() : Buffer {
+    const buffer = Buffer.alloc(1000);
+    this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer);
+    return buffer.slice(0 , this.borshInstructionSchema.getSpan(buffer));
+  }  
+
+  // deserializing the schema 
+  static deserialize(buffer ?: Buffer) : StudentIntro | null {
+    if(!buffer) {
+      return null;
+    }
+    try {
+      const { name , message } = this.borshAccountSchema.decode(buffer);
+      return new StudentIntro(name , message);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
 }
--- scripts/serialize/StudentIntroCoordinator.ts
+++ scripts/serialize/StudentIntroCoordinator.ts
@@ -1,4 +1,58 @@
+import { Connection, PublicKey } from "@solana/web3.js"
+import * as web3 from "@solana/web3.js"
+import base58 from "bs58";
+import { StudentIntro } from "../../models/serialize/StudentIntro";
+
+const STUDENT_INTRO_PROGRAM_ID = 'HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf'
 
 export class StudentIntroCoordinator {
+    static accounts : PublicKey[] = [];
+    static async prefetchAccounts(connection : Connection, search : string) {
+        const accounts = await connection.getProgramAccounts(
+            new web3.PublicKey(STUDENT_INTRO_PROGRAM_ID),
+            {
+                dataSlice: {
+                    offset : 1,
+                    length : 12,
+                },
+                filters : search === "" ? [] : [
+                    {
+                        memcmp: {
+                            offset: 5,
+                            bytes: base58.encode(Buffer.from(search)),
+                        }
+                    }
+                ]
+            }
+        );
 
+        accounts.sort((a,b) => {
+            const lengthA = a.account.data.readUInt32LE(0);
+            const lengthB = a.account.data.readUInt32LE(0);
+            const dataA = a.account.data.slice(4 , 4 + lengthA);
+            const dataB = b.account.data.slice(4 , 4 + lengthB);
+            return dataA.compare(dataB);
+        });
+
+        this.accounts = accounts.map(account => account.pubkey);
+    }
+
+    static async fetchPage(connection : Connection , page : number , perPage : number , search : string , reload : boolean = false) : Promise<StudentIntro[]> {
+        if (this.accounts.length === 0 || reload) {
+            await this.prefetchAccounts(connection , search);
+        }
+        const paginatedPublicKeys = this.accounts.slice((page - 1) * perPage , page * perPage);
+        if (!paginatedPublicKeys) {
+         return [] 
+        }
+        const accounts = await connection.getMultipleAccountsInfo(paginatedPublicKeys);
+        const studentIntros = accounts.reduce((accum: StudentIntro[] , account) => {
+            const studentIntro = StudentIntro.deserialize(account?.data);
+            if (!studentIntro) {
+                return accum;
+            }
+            return [...accum , studentIntro];
+        } , []);
+        return studentIntros;
+    }   
 };
